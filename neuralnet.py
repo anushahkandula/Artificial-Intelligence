f8f5ae7e9c92de3ca4376d4d37f7a71b0d83524b
anushahkandula <53189455+anushahkandula@users.noreply.github.com>
2019-11-22 11:32:33 -0600

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -1,16 +1,3 @@
-# neuralnet.py
-# ---------------
-# Licensing Information:  You are free to use or extend this projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to the University of Illinois at Urbana-Champaign
-#
-# Created by Justin Lizama (jlizama2@illinois.edu) on 10/29/2019
-"""
-This is the main entry point for MP6. You should only modify code
-within this file -- the unrevised staff files will be used for all other
-files and classes when code is run, so be careful to not modify anything else.
-"""
 
 import torch
 from torch import ones
@@ -18,22 +5,7 @@ import numpy as np
 
 class NeuralNet(torch.nn.Module):
     def __init__(self, lrate, loss_fn, in_size, out_size):
-        """
-        Initialize the layers of your neural network
-
-        @param lrate: The learning rate for the model.
-        @param loss_fn: A loss function defined in the following way:
-            @param yhat - an (N,out_size) tensor
-            @param y - an (N,) tensor
-            @return l(x,y) an () tensor that is the mean loss
-        @param in_size: Dimension of input
-        @param out_size: Dimension of output
-
-        For Part 1 the network should have the following architecture (in terms of hidden units):
-
-        in_size -> 32 ->  out_size
-
-        """
+       
         super(NeuralNet, self).__init__()
         self.loss_fn = loss_fn
         self.lrate = lrate
@@ -44,37 +16,21 @@ class NeuralNet(torch.nn.Module):
 
 
     def set_parameters(self, params):
-        """ Set the parameters of your network
-        @param params: a list of tensors containing all parameters of the network
-        """
+
         pass
 
     def get_parameters(self):
-        """ Get the parameters of your network
-        @return params: a list of tensors containing all parameters of the network
-        """
+
         return self.netmodel.parameters()
 
 
     def forward(self, x):
-        """ A forward pass of your neural net (evaluates f(x)).
-
-        @param x: an (N, in_size) torch tensor
-
-        @return y: an (N, out_size) torch tensor of output from the network
-        """
 
         x=(x-(x.mean(dim=1,keepdim=True)))/(x.std(dim=1,keepdim=True))
         return self.netmodel(x)
 
 
     def step(self, x, y):
-        """
-        Performs one gradient step through a batch of data x with labels y
-        @param x: an (N, in_size) torch tensor
-        @param y: an (N,) torch tensor
-        @return L: total empirical risk (mean of losses) at this time step as a float
-        """
 
         self.optimizer.zero_grad()
         loss = self.loss_fn(self.forward(x), y)
@@ -84,23 +40,7 @@ class NeuralNet(torch.nn.Module):
 
 
 def fit(train_set,train_labels,dev_set,n_iter,batch_size=1):
-    """ Make NeuralNet object 'net' and use net.step() to train a neural net
-    and net(x) to evaluate the neural net.
 
-    @param train_set: an (N, in_size) torch tensor
-    @param train_labels: an (N,) torch tensor
-    @param dev_set: an (M,) torch tensor
-    @param n_iter: int, the number of epochs of training
-    @param batch_size: The size of each batch to train on. (default 100)
-
-    # return all of these:
-
-    @return losses: Array of total loss at the beginning and after each iteration. Ensure len(losses) == n_iter
-    @return yhats: an (M,) NumPy array of binary labels for dev_set
-    @return net: A NeuralNet object
-
-    # NOTE: This must work for arbitrary M and N
-    """
     # train_set=(train_set-torch.mean(train_set))/torch.std(train_set)
     loss_function = torch.nn.CrossEntropyLoss()
     net = NeuralNet(0.00001, loss_function, len(train_set[0]), 2)
